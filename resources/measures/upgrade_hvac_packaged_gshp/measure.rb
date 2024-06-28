2a088a974829dbf90adce3c8d19d5e4a6e50cf18
Amy Allen <amy.allen@colorado.edu>
2024-06-28 10:54:01 -0600

    using Load based control

--- resources/measures/upgrade_hvac_packaged_gshp/measure.rb
+++ resources/measures/upgrade_hvac_packaged_gshp/measure.rb
@@ -612,7 +612,8 @@ class AddPackagedGSHP < OpenStudio::Measure::ModelMeasure
 
       # Create a new water-to-air ground source heat pump system
       unitary_system = OpenStudio::Model::AirLoopHVACUnitarySystem.new(model)
-      unitary_system.setControlType('Setpoint')
+      unitary_system.setControlType('Load')
+	  unitary_system.setControllingZoneorThermostatLocation(thermal_zone) 
       unitary_system.setCoolingCoil(new_cooling_coil)
       unitary_system.setHeatingCoil(new_heating_coil)
       unitary_system.setControllingZoneorThermostatLocation(thermal_zone)
